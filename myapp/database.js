4304335bd5e0229181e2670f48b2af33e45a29e4
Saurabh Agarwal <sagarwal144@bloomberg.net>
2020-06-07 17:52:50 -0400

    somil debate page with contact us form

--- myapp/database.js
+++ myapp/database.js
@@ -3,7 +3,7 @@ var conn = mysql.createConnection({
     host: "mysqldbserver.mysql.database.azure.com",
     user: "sonika@mysqldbserver",
     password: "Somildebatedb123",
-    database: 'somildebatedb'
+    database: 'somildebmysql'
 });
 conn.connect(function (err) {
     if (err) throw err;
